73a94ff59aed4ae7b15caaefef0b3503372f66f8
Tomas Gonzalez <tomas.g.2012@utexas.edu>
2016-04-05 11:25:10 -0500

    Extended FLASK implementation from Location.html
    Now works for category and restaurant requests, however the formatting is still temporary.

--- app/app.py
+++ app/app.py
@@ -13,10 +13,13 @@ def makerequest(item, num):
     if item == "location":
         return {'id': num, 'imageurl': '../img/austin-tx.jpg', 'address': 'Example', 'neighborhood': 'Example', 'zipcode': '45678-789', 'latitude': 4.5, 'longitude': 4.5, 'restaurant': 1}
     if item == "category":
-        return {'id': num, 'name': 'Example', 'restotal': 100, 'reviewtotal': 100, 'ratingavg': 4.5, 'restlist': [1,2,3]}
+        return {'id': num, 'name': 'Example', 'resttotal': 100, 'reviewtotal': 100, 'ratingavg': 4.5, 'restlist': [1,2,3]}
     if item == "restaurant":
         return {'id': num, 'imageurl': '../img/austin-tx.jpg', 'name': 'Example', 'phonenum': '(123)456-7890', 'rating': 4.5, 'reviewcount': 123, 'url': 'http://google.com', 'location_id': 1, "catlist": [1,2,3]}
 
+def requestsbyfilter(unknown):
+    # if need be
+    pass
 
 @app.route('/')
 def index():
@@ -24,6 +27,9 @@ def index():
 	
 @app.route('/location')
 def render_location():
+    # what do we do here, make 10 requests based on incrementing ids
+    # then paginate the rest of the requests at the bottom by default?
+    #   we have to make a specific routing for filter
     return render_template('location_db.html')
 	
 @app.route('/location/<location_id>')
@@ -38,18 +44,25 @@ def render_restaurant():
 	
 @app.route('/restaurant/<restaurant_id>')
 def render_restaurant_id(restaurant_id=None):
-	id = restaurant_id
-	return render_template('restaurant.html', id = id)
-	
+    resdict = makerequest("restaurant",restaurant_id);
+    locdict = makerequest("location",resdict['location_id']);
+    catlist = list();
+    for catnum in resdict['catlist']:
+        catlist.append(makerequest("category",catnum));
+    return render_template('restaurant.html', restdict = resdict, locdict = locdict, catlist = catlist)
+
 @app.route('/category')
 def render_category():
     return render_template('category_db.html')
-	
+
 @app.route('/category/<category_id>')
 def render_category_id(category_id=None):
-	id = category_id
-	return render_template('category.html', id = id)
-	
+    catdict = makerequest("category",category_id);
+    reslist = list();
+    for resnum in catdict['restlist']:
+        reslist.append(makerequest("restaurant",resnum));
+    return render_template('category.html', catdict = catdict, reslist = reslist)
+
 @app.route('/about')
 def render_about():
     return render_template('about.html')
