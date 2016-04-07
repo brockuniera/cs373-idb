00442a0faf006c0cad1a46b5ea3772d3bc1dcafe
zachsand <zachsand@utexas.edu>
2016-04-07 18:42:25 -0500

    Added dynamic sorting to smaller pages using angular, addied backend sorting for larger pages

--- app/app.py
+++ app/app.py
@@ -27,17 +27,39 @@ def index():
 @app.route('/location')
 def render_location():
     offset = getOffset(request.args.get('page'))
-    locDataDictList = getDataDictList(
-        Location.query.filter_by().limit(results_per_page).offset(offset))
-    return render_template('template_db.html', dataNames = Location.getDataNames(),
-        dataList = dumps(locDataDictList), title = "Locations", page = "location")
+    sortby = validateSortString(request.args.get('sortby'), Location)
+    direction = validateDirectionString(request.args.get('dir'))
+
+    if sortby is not None:
+        if direction == "ascending":
+            locDataDictList = getDataDictList(
+                Location.query.order_by(getattr(Location, sortby)).all()
+            )
+        else:
+            locDataDictList = getDataDictList(
+                Location.query.order_by(getattr(Location, sortby).desc()).all()
+            )
+    else:
+        locDataDictList = Location.query.filter_by().limit(results_per_page).offset(offset)
+
+    return render_template('template_db.html',
+            dataNames=Location.getDataNames(),
+            dataList=locDataDictList,
+            title="Locations",
+            page="location",
+            direction=direction
+        )
 
 @app.route('/location/<location_id>')
 def render_location_id(location_id=None):
-    locModel = Location.query.get(location_id)
+    locModel = Location.query.get_or_404(location_id)
     relatedRestModel = Restaurant.query.filter_by(location_id = location_id).one()
-    return render_template('location.html', locModel = locModel, restModel = relatedRestModel)
-
+    catListModels = getDataDictList(relatedRestModel.catlist)
+    return render_template('location.html', 
+        locModel = locModel, 
+        restModel = relatedRestModel,
+        catAttrs = Category.getDataNames(),
+        catListModels = dumps(catListModels))
 # Restaurant
 
 @app.route('/restaurant')
@@ -45,47 +67,83 @@ def render_restaurant():
     # Get query string args
     offset = getOffset(request.args.get('page'))
     sortby = validateSortString(request.args.get('sortby'), Restaurant)
-    logger.debug(sortby)
+    direction = validateDirectionString(request.args.get('dir'))
 
-    restDataDictList = getDataDictList(
-        Restaurant.query.order_by(sortby and getattr(Restaurant, sortby)).all()
-    )
+    if sortby is not None:
+        if direction == "ascending":
+            restDataDictList = getDataDictList(
+                Restaurant.query.order_by(getattr(Restaurant, sortby)).all()
+            )
+        else:
+            restDataDictList = getDataDictList(
+                Restaurant.query.order_by(getattr(Restaurant, sortby).desc()).all()
+            )
+    else:
+        restDataDictList = Restaurant.query.filter_by().limit(results_per_page).offset(offset)
 
     return render_template('template_db.html',
             dataNames=Restaurant.getDataNames(),
             dataList=restDataDictList,
             title="Restaurants",
-            page="restaurant"
+            page="restaurant",
+            direction=direction
         )
 
 @app.route('/restaurant/<restaurant_id>')
 def render_restaurant_id(restaurant_id=None):
     restModel = Restaurant.query.get_or_404(restaurant_id)
-    relatedLocModel = Location(id = restModel.location_id).one()
-    return render_template('restaurant.html', restModel = restModel, locModel = relatedLocModel)
+    relatedLocModel = Location.query.get(restModel.location_id)
+    catListModels = getDataDictList(restModel.catlist)
+    return render_template('restaurant.html', 
+        restModel = restModel, 
+        locModel = relatedLocModel,
+        catAttrs = Category.getDataNames(),
+        catListModels = dumps(catListModels))
 
 # Category
 
 @app.route('/category')
 def render_category():
+    # Get query string args
     offset = getOffset(request.args.get('page'))
-    catDataDictList = getDataDictList(
-        Category.query.filter_by().limit(results_per_page).offset(offset))
-    return render_template('template_db.html', dataNames = Category.getDataNames(),
-        dataList = dumps(catDataDictList), title = "Categories", page = "category")
+    sortby = validateSortString(request.args.get('sortby'), Category)
+    direction = validateDirectionString(request.args.get('dir'))
+
+    if sortby is not None:
+        if direction == "ascending":
+            catDataDictList = getDataDictList(
+                Category.query.order_by(getattr(Category, sortby)).all()
+            )
+        else:
+            catDataDictList = getDataDictList(
+                Category.query.order_by(getattr(Category, sortby).desc()).all()
+            )
+    else:
+        catDataDictList = Category.query.filter_by().limit(results_per_page).offset(offset)
+
+    return render_template('template_db.html',
+            dataNames=Category.getDataNames(),
+            dataList=catDataDictList,
+            title="Categories",
+            page="category",
+            direction=direction
+        )
 
 @app.route('/category/<category_id>')
 def render_category_id(category_id=None):
-    catModel = Category.query.get(category_id)
-    locModelList = list()
     imgList = list()
     imgIndex = 1
+    catModel = Category.query.get_or_404(category_id)
     for restModel in catModel.restlist:
         if(imgIndex <= 5):
             imgList.append((imgIndex, restModel.imageurl))
-        locModelList.append(Location.query.get(restModel.location_id))
-    return render_template('category.html', catModel = catModel, locModelList = locModelList,
-        imgList = imgList)
+
+    relatedRestModels = getDataDictList(catModel.restlist)
+    return render_template('category.html', 
+        catModel = catModel, 
+        restAttrs = Restaurant.getDataNames(),
+        imgList = imgList,
+        restListModels = dumps(relatedRestModels))
 
 @app.route('/about')
 def render_about():
@@ -114,6 +172,15 @@ def validateSortString(sortstring, classmodel):
     return None if sortstring is not a (sortable?) attribute in classmodel
     """
     return sortstring if sortstring in classmodel.getDataNames() else None
+
+def validateDirectionString(querydirection):
+    direction = querydirection or "ascending"
+    if direction == "ascending":
+        direction = "descending"
+    else:
+        direction = "ascending"
+    return direction
+
 
 if __name__ == '__main__':
     app.debug = True
