06c220672c2a4dca020ce42c70eb93cfae861cf4
zachsand <zachsand@utexas.edu>
2016-04-08 16:28:59 -0500

    Fixed API issues, regenerated models.html

--- app/api.py
+++ app/api.py
@@ -19,33 +19,53 @@ def api_root():
 @app.route('/api/restaurant')
 def api_list_restaurants():
     "List all of our restaurants"
-    return jsonify(restaurantlist={i.id : i.__dict__ for i in Restaurant.query.all()})
+    dicts = getDataDicts(Restaurant.query.filter_by().all())
+    return jsonify(restaurantList=dicts)
 
 @app.route('/api/restaurant/<int:id>')
 def api_get_restaurant(id):
     "List a specific restaurant by specifying an id"
-    return dumps(Restaurant.query.get(id).__dict__)
+    return dumps(getDataDict(Restaurant.query.get(id)))
 
 # Categories
 
 @app.route('/api/category')
 def api_list_categories():
     "List all of our categories"
-    return jsonify(categorylist={i.id : i.__dict__ for i in Category.query.all()})
+    dicts = getDataDicts(Category.query.filter_by().all())
+    return jsonify(categorylist=dicts)
 
 @app.route('/api/category/<int:id>')
 def api_get_category(id):
     "List a specific category by specifying an id"
-    return dumps(Category.query.get(id).__dict__)
+    return dumps(getDataDict(Category.query.get(id)))
 
 # Locations
 
 @app.route('/api/location')
 def api_list_locations():
     "List all of our locations"
-    return jsonify(locationlist={i.id : i.__dict__ for i in Location.query.all()})
+    dicts = getDataDicts(Location.query.filter_by().all())
+    return jsonify(locationlist=dicts)
 
 @app.route('/api/location/<int:id>')
 def api_get_location(id):
     "List a specific location by specifying an id"
-    return dumps(Location.query.get(id).__dict__)
+    return dumps(getDataDict(Location.query.get(id)))
+
+def getDataDicts(modelList):
+    """
+    Returns a dictionary of dictionaries based on modelList, key is id value is dict
+    """
+    dataDicts = {}
+    for modelObj in modelList:
+        dataDicts[modelObj.id] = getDataDict(modelObj)
+    return dataDicts
+
+def getDataDict(model):
+    """
+    Returns a single dictionary of model without sqlalchemy instance key in it
+    """
+    dataDict = model.__dict__
+    dataDict.pop("_sa_instance_state", None)
+    return dataDict
